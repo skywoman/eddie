75634daba6a3be7e673ff747da00c196d6e4b120
Chris Newman <chris@sylvanaqua.com>
2021-12-21 16:33:18 -0500

    Filtering of mutual aid orders, etc.

--- readsquare.js
+++ readsquare.js
@@ -1,3 +1,5 @@
+// To run: node -r esm readsquare.js
+
 import { Client, Environment } from 'square'
 
 const client = new Client({
@@ -7,22 +9,116 @@ const client = new Client({
 
 const ordersApi = client.ordersApi;
 
-(async function() {
+var map = {};
+var totalPurchaseValue = 0.0;
+
+async function searchOrders () {
+
+	var resultsArray = new Array();
+	var cursorPresent = '';
+
+	do{
+		try {
+		  const response = await client.ordersApi.searchOrders({
+		    locationIds: [
+		      'AB4HBYP24XMZW'
+		    ],
+		    query: {
+		      filter: {
+		        stateFilter: {
+		          states: [
+		            'OPEN'
+		          ]
+		        },
+		        dateTimeFilter: {
+		          createdAt: {
+		            startAt: '2021-10-24T00:00:00+00:00'
+		          }
+		        }
+		      }
+		    },
+		    limit: 500,
+		    returnEntries: false,
+		    cursor: cursorPresent
+		  });
+
+		  if(response.result.cursor){
+		  	cursorPresent = response.result.cursor;	  	
+		  }
+		  else{
+		  	cursorPresent = '';
+		  }
+
+		  resultsArray.push(response.result);
+
+		} catch(error) {
+		  console.log(error);
+		}
+	} while (cursorPresent != '');
+
+	return resultsArray;
+	
+};
+
+
+function addLineItemsToMap(order){
+
 	try {
-	  const response = await client.ordersApi.searchOrders({
-	    locationIds: [
-	      'AB4HBYP24XMZW'
-	    ],
-	    limit: 10,
-	    returnEntries: true
-	  });
 
-	  response.result.orderEntries.forEach(function (order) {
-	  	console.log(order.orderId);
-	  });
+		const lineItems = order.lineItems;
+		lineItems.forEach(function(lineItem) {
+			if(!lineItem.name.toUpperCase().includes("MUTUAL") && 
+				 !lineItem.name.toUpperCase().includes("WHOLESALE") &&
+				 !lineItem.name.toUpperCase().includes("CASE") &&
+				 !lineItem.name.toUpperCase().includes("PER LB") &&
+				 !lineItem.name.toUpperCase().includes("PRIMAL") &&
+				 !lineItem.name.toUpperCase().includes("DELIVERY") &&
+				 !lineItem.name.toUpperCase().includes("HALF HOG")){
 
+				// Add purchase value to the total
+				totalPurchaseValue += (parseFloat(lineItem.totalMoney.amount) / 100.00);
 
-	} catch(error) {
-	  console.log(error);
+				if(lineItem.name in map){
+					map[lineItem.name] = map[lineItem.name]+parseInt(lineItem.quantity);
+				}
+				else{
+					map[lineItem.name] = parseInt(lineItem.quantity);
+				}
+			}
+		});
+	} catch (error) {
+		console.log(error);
 	}
-}());
+}
+
+async function main() {
+	const ordersResultsArray = await searchOrders();
+
+	for(let h = 0; h < ordersResultsArray.length; h++){
+
+		var ordersResult = ordersResultsArray[h];
+		if(ordersResult.orders){
+			for (let i = 0; i < ordersResult.orders.length; i++){
+				var order = ordersResult.orders[i];
+				addLineItemsToMap(order);
+			}
+		}
+	}
+
+	finishUp();
+}
+
+function finishUp(){
+	console.log(sortObjectByKeys(map));
+	console.log("\n");
+	console.log("TOTAL VALUE: $" + totalPurchaseValue.toFixed(2));
+	console.log("\n");
+}
+
+function sortObjectByKeys(o) {
+    return Object.keys(o).sort().reduce((r, k) => (r[k] = o[k], r), {});
+}
+
+main();
+
+
